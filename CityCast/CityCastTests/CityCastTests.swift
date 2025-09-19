dc0864710df8b3655b0880956c91f238da7ddc41
Rushikesh Suradhkar <rushikeshsuradkar2000@gmail.com>
2025-09-19 22:15:22 +0530

    default project commit

---
+++ CityCast/CityCastTests/CityCastTests.swift
@@ -0,0 +1,17 @@
+//
+//  CityCastTests.swift
+//  CityCastTests
+//
+//  Created by Rushikesh Suradkar on 19/09/25.
+//
+
+import Testing
+@testable import CityCast
+
+struct CityCastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
